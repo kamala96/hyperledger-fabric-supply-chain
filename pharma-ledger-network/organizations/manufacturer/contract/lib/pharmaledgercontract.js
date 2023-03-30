464caeee79dd2536ac69a13248e5e0f5fca1106e
kamala96 <jovinjohn959@gmail.com>
2023-03-30 11:23:12 +0300

    Last week training

--- pharma-ledger-network/organizations/manufacturer/contract/lib/pharmaledgercontract.js
+++ pharma-ledger-network/organizations/manufacturer/contract/lib/pharmaledgercontract.js
@@ -47,7 +47,7 @@ class PharmaLedgerContract extends Contract {
      * @param {String} equipment name
      * @param {String} name of the equipment owner
     */
-   async makeEquipment(ctx, manufacturer, equipmentNumber, equipmentName, ownerName) {
+    async makeEquipment(ctx, manufacturer, equipmentNumber, equipmentName, ownerName) {
         console.info('============= START : makeEquipment call ===========');
         let dt = new Date().toString();
         const equipment = {
@@ -62,15 +62,15 @@ class PharmaLedgerContract extends Contract {
         };
         await ctx.stub.putState(equipmentNumber, Buffer.from(JSON.stringify(equipment)));
         console.info('============= END : Create equipment ===========');
-   }
-   /**
-     * Manufacturer send equipment To Wholesaler
-     *
-     * @param {Context} ctx the transaction context
-     * @param {String} equipmentNumber for this equipment
-     * @param {String} name of the equipment owner
-   */
-   async wholesalerDistribute(ctx, equipmentNumber, ownerName) {
+    }
+    /**
+      * Manufacturer send equipment To Wholesaler
+      *
+      * @param {Context} ctx the transaction context
+      * @param {String} equipmentNumber for this equipment
+      * @param {String} name of the equipment owner
+    */
+    async wholesalerDistribute(ctx, equipmentNumber, ownerName) {
         console.info('============= START : wolesalerDistribute call ===========');
         const equipmentAsBytes = await ctx.stub.getState(equipmentNumber);
         if (!equipmentAsBytes || equipmentAsBytes.length === 0) {
@@ -81,10 +81,10 @@ class PharmaLedgerContract extends Contract {
         let record;
         try {
             record = JSON.parse(strValue);
-            if(record.currentOwnerType!=='MANUFACTURER') {
-               throw new Error(` equipment - ${equipmentNumber} owner must be MANUFACTURER`);
+            if (record.currentOwnerType !== 'MANUFACTURER') {
+                throw new Error(` equipment - ${equipmentNumber} owner must be MANUFACTURER`);
             }
-            record.previousOwnerType= record.currentOwnerType;
+            record.previousOwnerType = record.currentOwnerType;
             record.currentOwnerType = 'WHOLESALER';
             record.ownerName = ownerName;
             record.lastUpdated = dt;
@@ -94,15 +94,15 @@ class PharmaLedgerContract extends Contract {
         }
         await ctx.stub.putState(equipmentNumber, Buffer.from(JSON.stringify(record)));
         console.info('============= END : wolesalerDistribute  ===========');
-   }
-   /**
-     * Wholesaler send equipment To Pharmacy
-     *
-     * @param {Context} ctx the transaction context
-     * @param {String} equipmentNumber for this equipment
-     * @param {String} name of the equipment owner
-   */
-   async pharmacyReceived(ctx, equipmentNumber, ownerName) {
+    }
+    /**
+      * Wholesaler send equipment To Pharmacy
+      *
+      * @param {Context} ctx the transaction context
+      * @param {String} equipmentNumber for this equipment
+      * @param {String} name of the equipment owner
+    */
+    async pharmacyReceived(ctx, equipmentNumber, ownerName) {
         console.info('============= START : pharmacyReceived call ===========');
         const equipmentAsBytes = await ctx.stub.getState(equipmentNumber);
         if (!equipmentAsBytes || equipmentAsBytes.length === 0) {
@@ -114,10 +114,10 @@ class PharmaLedgerContract extends Contract {
         try {
             record = JSON.parse(strValue);
             //make sure owner is wholesaler
-            if(record.currentOwnerType!=='WHOLESALER') {
-               throw new Error(` equipment - ${equipmentNumber} owner must be WHOLESALER`);
+            if (record.currentOwnerType !== 'WHOLESALER') {
+                throw new Error(` equipment - ${equipmentNumber} owner must be WHOLESALER`);
             }
-            record.previousOwnerType= record.currentOwnerType;
+            record.previousOwnerType = record.currentOwnerType;
             record.currentOwnerType = 'PHARMACY';
             record.ownerName = ownerName;
             record.lastUpdated = dt;
@@ -127,42 +127,42 @@ class PharmaLedgerContract extends Contract {
         }
         await ctx.stub.putState(equipmentNumber, Buffer.from(JSON.stringify(record)));
         console.info('============= END : pharmacyReceived  ===========');
-   }
-   /**
-     * query ledger record By Key
-     *
-     * @param {Context} ctx the transaction context
-     * @param {String} key for record
-   */
-   async queryByKey(ctx, key) {
+    }
+    /**
+      * query ledger record By Key
+      *
+      * @param {Context} ctx the transaction context
+      * @param {String} key for record
+    */
+    async queryByKey(ctx, key) {
         let value = await ctx.stub.getState(key);
         const strValue = Buffer.from(value).toString('utf8');
         let record;
-            try {
-                record = JSON.parse(strValue);
-            } catch (err) {
-                console.log(err);
-                record = strValue;
-            }
-        return JSON.stringify({
-           Key: key, Record: record
-        });
-   }
-   async queryHistoryByKey(ctx, key) {
-      console.info('getting history for key: ' + key);
-      let iterator = await ctx.stub.getHistoryForKey(key);
-      let result = [];
-      let res = await iterator.next();
-      while (!res.done) {
-        if (res.value) {
-          const obj = JSON.parse(res.value.value.toString('utf8'));
-          result.push(obj);
+        try {
+            record = JSON.parse(strValue);
+        } catch (err) {
+            console.log(err);
+            record = strValue;
         }
-        res = await iterator.next();
-      }
-      await iterator.close();
-      console.info(result);
-      return JSON.stringify(result);
-  }
+        return JSON.stringify({
+            Key: key, Record: record
+        });
+    }
+    async queryHistoryByKey(ctx, key) {
+        console.info('getting history for key: ' + key);
+        let iterator = await ctx.stub.getHistoryForKey(key);
+        let result = [];
+        let res = await iterator.next();
+        while (!res.done) {
+            if (res.value) {
+                const obj = JSON.parse(res.value.value.toString('utf8'));
+                result.push(obj);
+            }
+            res = await iterator.next();
+        }
+        await iterator.close();
+        console.info(result);
+        return JSON.stringify(result);
+    }
 }
 module.exports = PharmaLedgerContract;
